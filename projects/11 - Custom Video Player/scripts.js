c81776bf83af11904c597abc8c6c8dbfbc2283aa
serhatcigerci <yserhatcigerci@gmail.com>
2022-04-13 23:54:31 +0300

    after 11

--- projects/11 - Custom Video Player/scripts.js
+++ projects/11 - Custom Video Player/scripts.js
@@ -22,7 +22,35 @@ function updateButton() {
     toggle.textContent = icon
 }
 
+function skip() {
+    video.currentTime += parseInt(this.dataset.skip)
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value
+    video[this.name] = this.value
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100
+    progressBar.style.flexBasis = `${percent}%`
+}
+
+function scrub(e) {
+const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration
+}
+
 video.addEventListener('click', togglePlay)
 video.addEventListener('play', togglePlay)
 video.addEventListener('pause', togglePlay)
+video.addEventListener('timeupdate', handleProgress)
 toggle.addEventListener('click', togglePlay)
+skipButtons.forEach(button => button.addEventListener('click', skip))
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
+
+let mousedown = false
+
+progress.addEventListener('click', scrub)
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e))
+progress.addEventListener('mousedown', () => mousedown = true)
+progress.addEventListener('mouseup', () => mousedown = false)
