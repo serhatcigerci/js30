3b12be253f5768fb030d30eaef1f37e46f77d405
serhatcigerci <yserhatcigerci@gmail.com>
2022-04-30 15:59:58 +0300

    red effect

--- projects/19 - Webcam Fun/scripts.js
+++ projects/19 - Webcam Fun/scripts.js
@@ -27,6 +27,9 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height)
+    let pixels = ctx.getImageData(0, 0, width, height)
+    pixels = redEffect(pixels)
+    ctx.putImageData(pixels, 0, 0)
   }, 16)
 }
 
@@ -38,8 +41,17 @@ function takePhoto() {
   const link = document.createElement('a')
   link.href = data
   link.setAttribute('download', 'the-photo')
-  link.textContent = 'Download Image'
+  link.innerHTML = `<img src = "${data}" alt="my man"/>`
   strip.insertBefore(link, strip.firstChild)
+}
+
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100
+    pixels.data[i + 1] = pixels.data[i + 1] - 50
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5
+  }
+  return pixels
 }
 
 getVideo()
