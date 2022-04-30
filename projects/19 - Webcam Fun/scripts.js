eca51fabeb47e4fbd97307d0f8306a52f3596ba8
serhatcigerci <yserhatcigerci@gmail.com>
2022-04-30 16:03:19 +0300

    rgb effect

--- projects/19 - Webcam Fun/scripts.js
+++ projects/19 - Webcam Fun/scripts.js
@@ -28,7 +28,8 @@ function paintToCanvas() {
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height)
     let pixels = ctx.getImageData(0, 0, width, height)
-    pixels = redEffect(pixels)
+  //  pixels = redEffect(pixels)
+  pixels = rgbSplit(pixels)
     ctx.putImageData(pixels, 0, 0)
   }, 16)
 }
@@ -50,6 +51,15 @@ function redEffect(pixels) {
     pixels.data[i + 0] = pixels.data[i + 0] + 100
     pixels.data[i + 1] = pixels.data[i + 1] - 50
     pixels.data[i + 2] = pixels.data[i + 2] * 0.5
+  }
+  return pixels
+}
+
+function rgbSplit(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i + 0]
+    pixels.data[i + 100] = pixels.data[i + 1]
+    pixels.data[i - 150] = pixels.data[i + 2]
   }
   return pixels
 }
