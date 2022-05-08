762d5026c9fb871e273c0168b83fde55588957b6
serhatcigerci <yserhatcigerci@gmail.com>
2022-05-08 15:33:31 +0300

    moles come out

---
+++ projects/30 - Whack a Mole!/script.js
@@ -0,0 +1,29 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min)
+}
+
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length)
+  const hole = holes[idx]
+  if (hole === lastHole) {
+    return randomHole
+  }
+
+  lastHole = hole
+  return hole
+}
+
+function peep() {
+  const time = randomTime(200, 1000)
+  const hole = randomHole(holes)
+  hole.classList.add('up')
+  setTimeout(() => {
+    hole.classList.remove('up')
+    if (!timeUp) peep()
+  }, time)
+}
